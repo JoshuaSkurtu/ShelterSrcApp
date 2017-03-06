4a085b7905326f1f5b80f5b0c7fe2365d7929532
MasterOfCodes <htp200006@gmail.com>
2017-03-06 17:10:47 -0600

    remade layout, formatted button, added double password encryption, email, lastname,

--- app/src/main/java/net/androidbootcamp/sheltersrcapp/RegisterActivity.java
+++ app/src/main/java/net/androidbootcamp/sheltersrcapp/RegisterActivity.java
@@ -19,6 +19,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
@@ -26,36 +27,49 @@ public class RegisterActivity extends AppCompatActivity {
         getSupportActionBar().setLogo(R.mipmap.ic_launcher);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
 
+
+        final EditText etEmail = (EditText)findViewById(R.id.etEmail);
         final EditText etAge = (EditText) findViewById(R.id.etAge);
         final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etLName = (EditText) findViewById(R.id.etLName);
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etPassword2 = (EditText) findViewById(R.id.etPassword2);
         final Button bRegister = (Button) findViewById(R.id.bRegister);
 
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //final int age = Integer.parseInt(etAge.getText().toString());
+                final int age = Integer.parseInt("0" + etAge.getText().toString());
+                final String email = etEmail.getText().toString();
                 final String name = etName.getText().toString();
+                final String lname = etLName.getText().toString();
                 final String username = etUsername.getText().toString();
                 final String password = etPassword.getText().toString();
-                final int age = Integer.parseInt(etAge.getText().toString());
+                final String password2 = etPassword2.getText().toString();
 
-                Response.Listener<String> responseListner = new Response.Listener<String>(){
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
                     @Override
                     public void onResponse(String response) {
                         try {
+                            //gets JSONObject from php file
                             JSONObject jsonResponse = new JSONObject(response);
+                            //gets if connection is successful
                             boolean success = jsonResponse.getBoolean("success");
 
+                            //id success==true from json php file then proceed
                             if(success){
                                 Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                                 RegisterActivity.this.startActivity(intent);
                             }else{
                                 AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                builder.setMessage("Register Failed")
+                                builder.setMessage("Register Failed, Please try again.")
                                         .setNegativeButton("Retry", null)
                                         .create()
                                         .show();
+
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -63,7 +77,8 @@ public class RegisterActivity extends AppCompatActivity {
                     }
                 };
 
-                RegisterRequest registerRequest = new RegisterRequest(name, username, age, password, responseListner);
+                //this sends all the data
+                RegisterRequest registerRequest = new RegisterRequest(name, lname, username, age, password, password2, email, responseListener);
                 RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
                 queue.add(registerRequest);
             }
