92ba024493a10ce764a9308ba194ff950d5dd670
MasterOfCodes <htp200006@gmail.com>
2017-03-06 17:52:30 -0600

    editted the double password, email, lastname, password, encryption, working file

--- app/src/main/java/net/androidbootcamp/sheltersrcapp/RegisterRequest.java
+++ app/src/main/java/net/androidbootcamp/sheltersrcapp/RegisterRequest.java
@@ -15,13 +15,19 @@ public class RegisterRequest extends StringRequest{
     private static final String REGISTER_REQUEST_URL = "https://haitphan.000webhostapp.com/Register.php";
     private Map<String, String> params;
 
-    public RegisterRequest(String name, String username, int age, String password, Response.Listener<String> listener){
+    //makes Object RegisterRequest that holds all the parameters of the EditText
+    public RegisterRequest(String name, String lname, String username, int age, String password, String password2, String email, Response.Listener<String> listener){
         super(Method.POST, REGISTER_REQUEST_URL, listener, null);
         params = new HashMap<>();
+
+        //put("nameThatWillbeSentToPHPFileOnServer", variable name in RegisterRequest)
         params.put("name", name);
         params.put("username", username);
         params.put("password", password);
+        params.put("password2", password2);
         params.put("age", age + "");
+        params.put("lname", lname);
+        params.put("email", email);
     }
 
     @Override
