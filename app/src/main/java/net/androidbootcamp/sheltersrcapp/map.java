91231c755daac55dd788794d3c4e41d82fbbe9f0
JoshuaSkurtu <joshuaskurtu@gmail.com>
2017-03-17 12:27:02 -0500

    stable build-- messing with map so I wanted a save point - josh

--- app/src/main/java/net/androidbootcamp/sheltersrcapp/map.java
+++ app/src/main/java/net/androidbootcamp/sheltersrcapp/map.java
@@ -1,16 +1,23 @@
 package net.androidbootcamp.sheltersrcapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.design.widget.FloatingActionButton;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -21,6 +28,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.util.List;
 
 public class map extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -40,23 +51,25 @@ public class map extends AppCompatActivity implements OnMapReadyCallback {
 
         //adding drawer button - Josh
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close );
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
-        //more for button - Josh
-        @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-            if(mToggle.onOptionsItemSelected(item)){
-                return true;
 
-            }
+    //more for button - Josh
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+
+        }
 
         return super.onOptionsItemSelected(item);
     }
+
     //makes back buton close drawer - Josh
     @Override
     public void onBackPressed() {
@@ -67,22 +80,44 @@ public class map extends AppCompatActivity implements OnMapReadyCallback {
             super.onBackPressed();
         }
     }
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+    public void onSearch(View view) throws IOException {
+        EditText location_new = (EditText)findViewById(R.id.zip_search);
+        String location = location_new.getText().toString();
+        List<Address> addressList = null;
+        if(location != null || !location.equals("") )
+        {
+            Geocoder geocoder = new Geocoder(this);
+           try {
+              addressList = geocoder.getFromLocationName(location, 1);
+           }
+           catch(IOException e){
+               e.printStackTrace();
+           }
+           Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        }
+
+    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
     }
 }
