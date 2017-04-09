f4f89b6eb4b6c6236a5a9266dfeda9fcbff5911d
Hai Phan <htp200006@gmail.com>
2017-04-09 10:49:16 -0500

    Changed activity_provider_area.xml format and Added logo to ProviderAreaActivity.java

--- app/src/main/java/net/androidbootcamp/sheltersrcapp/ProviderAreaActivity.java
+++ app/src/main/java/net/androidbootcamp/sheltersrcapp/ProviderAreaActivity.java
@@ -9,5 +9,13 @@ public class ProviderAreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_area);
+
+        //logo- Hai
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        //Grab the textView message and Editext from xml to edit with response from ProviderAuthenticationActivity.java using intent from intent.putExtra - Hai
+
     }
 }
